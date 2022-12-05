8bfa449f7032a76cc57e8c05dc353931ab820526
DanilVelichko <velichko.danil@gmail.com>
2022-12-05 20:07:30 +0100

    Task 01 - done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//* Gallery rendering creation 
+const galleryContainer = document.querySelector('.gallery');
+
+function createPhotoRender(gallery) {
+    
+    return gallery.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+        <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        
+        `; 
+    }).join(''); 
+};
+
+const cardsMarkup = createPhotoRender(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+//* ------------------ Foto action -------------------- //
+galleryContainer.addEventListener('click', onGalleryContainerClick); // put CLick listener
+
+function onGalleryContainerClick(e) {
+    e.preventDefault(); // Prevent link loading
+
+    // prevent click on no-image
+    const isImageGalleryEl = e.target.classList.contains('gallery__image');
+    if (!isImageGalleryEl || e.target.nodeName !=="IMG")  { 
+        return;
+    } 
+    
+    // connect Lightbox library
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="1280" >
+`);
+    instance.show();
+
+    // close IMG
+    galleryContainer.addEventListener("keydown", (e) => {
+        if (e.code === "Escape") {
+            instance.close();
+        };
+    });
+    console.dir(e.target.dataset.source);
+};
+
+console.log(createPhotoRender(galleryItems));
+
